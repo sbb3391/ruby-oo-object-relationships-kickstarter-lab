981be7efe7b4fd0165add806025c7781d2417a7b
sbb3391 <sbb33391@gmail.com>
2020-12-05 22:34:57 -0500

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,50 @@
-class Backer
+# class Backer
 
+#     attr_accessor :name
+
+#     @@all = []
+
+#     def initialize(name)
+#         @name = name
+#         save
+#     end
+
+#     def self.all
+#         @@all
+#     end
+
+#     def save
+#         Backer.all << self
+#     end
+
+#     def back_project(project)
+#         ProjectBacker.new(project, self)
+#     end
+
+#     def backed_projects
+#         a = ProjectBacker.all.select { |pb| pb.backer == self}
+#         a.map {|i| i.project}
+#     end
+
+
+
+# end
+
+
+class Backer
+ 
     attr_accessor :name
 
     @@all = []
-
+     
     def initialize(name)
-        @name = name
-        save
+    @name = name 
+    # @backed_projects = []  -- the empty array returning all needs to be a class variable. I believe its 
+    # usually @@all
+    save
     end
+
+    # I added the save method and .all class method
 
     def self.all
         @@all
@@ -18,14 +55,24 @@ class Backer
     end
 
     def back_project(project)
+        # all this wants is to create a new Project Backer
+        # @backed_projects << project  
+        # project.backer << self
+
         ProjectBacker.new(project, self)
-    end
 
+    end
+    
     def backed_projects
-        a = ProjectBacker.all.select { |pb| pb.backer == self}
-        a.map {|i| i.project}
+        #binding.pry 
+
+       a = ProjectBacker.all.select { |project_backer| project_backer.backer == self}
+       a.map do |i| 
+        i.project 
+       end
+    end  
+
+    def add_backer(backer)
+    
     end
-
-
-
 end
